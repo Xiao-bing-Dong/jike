3f0f744b9246e4f81a03a70e3af2cd9e7f90bef0
Xiao-bing-Dong <2309878250@qq.com>
2026-03-13 23:46:33 +0800

    封装axios

---
+++ src/utils/request.js
@@ -0,0 +1,30 @@
+//axios的封装处理
+import axios from "axios";
+//1.根域名配置
+//2.超时时间
+//3.请求拦截器/响应拦截器
+
+export const request=axios.create({
+    baseURL: 'http://geek.itheima.net/v1_0',
+    timeout: 5000
+})
+
+//添加请求拦截器
+//在请求发送之前进行拦截，然后插入一些自定义的配置（比如参数的处理）
+request.interceptors.request.use((config)=>{
+    return config;
+},(error)=>{
+    return Promise.reject(error);
+})
+
+//添加响应拦截器
+//在响应返回客户端之前进行拦截，重点处理返回的数据
+request.interceptors.response.use((response)=>{
+    //2xx范围内的状态码都会触发该函数
+    //对响应数据作出处理
+    return response.data;
+},(error)=>{
+    //超出2xx范围的状态码都会触发该函数
+    //对响应数据作出处理
+    return Promise.reject(error);
+})

9598476f4d879c165a09bdfa7ed783bd348f26c6
Xiao-bing-Dong <2309878250@qq.com>
2026-03-27 21:28:02 +0800

    打包路由懒加载优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,35 @@
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 
-import {createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { AuthRoute } from '@/components/AuthRoute';
-import Home from "@/pages/Home";
-import Artical from "@/pages/Artical";
-import Publish from "@/pages/Publish";
+// import Home from "@/pages/Home";
+// import Artical from "@/pages/Artical";
+// import Publish from "@/pages/Publish";
+import { lazy,Suspense } from "react";
+
+//路由懒加载
+//1.lazy函数对组件进行导入
+const Home = lazy(() => import('@/pages/Home'));
+const Article = lazy(() => import('@/pages/Article'));
+const Publish = lazy(() => import('@/pages/Publish'))
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <AuthRoute><Layout /></AuthRoute>,
-        children:[
+        children: [
             {
-                path:'',
-                element: <Home />
+                path: '',
+                element: <Suspense fallback={'加载中'} ><Home /></Suspense>
             },
             {
-                path:'artical',
-                element: <Artical />
+                path: 'artical',
+                element: <Suspense fallback={'加载中'} ><Article /></Suspense>
             },
             {
-                path:'publish',
-                element: <Publish />
+                path: 'publish',
+                element: <Suspense fallback={'加载中'} ><Publish /></Suspense>
             }
         ]
     },
